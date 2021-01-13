7e2b44cb808c56d396fd122282725ec0e793b976
riz-ahmed <rzwn.hmd@gmail.com>
2021-01-13 17:58:47 +0100

    tests using decorator functions

--- src/test_math_func.py
+++ src/test_math_func.py
@@ -1,14 +1,18 @@
 import math_func
 import pytest
+import sys
 
+@pytest.mark.skipif(sys.version_info < (4,0), reason="This test wil be skipped")                                     # using decorator function from pytest library
 def test_add():
     assert math_func.add(7,3) == 10
     assert math_func.add(7) == 9
     assert math_func.add(5) == 7
 
+@pytest.mark.number
 def test_product():
     assert math_func.product(2) == 4
 
+@pytest.mark.strings                                    # using decorator fucntion from pytest for strings
 def test_add_strings():
     result = math_func.add("Hello ", "World")
     assert result == "Hello World"
