84a72fcb62c60fdfd176b29bb6f76fbbd302520d
riz-ahmed <rzwn.hmd@gmail.com>
2021-01-13 18:10:39 +0100

    passing parameters into test cases

--- src/test_math_func.py
+++ src/test_math_func.py
@@ -2,6 +2,7 @@ import math_func
 import pytest
 import sys
 
+"""
 @pytest.mark.skipif(sys.version_info < (4,0), reason="This test wil be skipped")                                     # using decorator function from pytest library
 def test_add():
     assert math_func.add(7,3) == 10
@@ -18,3 +19,15 @@ def test_add_strings():
     assert result == "Hello World"
     assert type(result) is str
     assert "Hwllod" not in result
+"""
+
+@pytest.mark.parametrize('num1, num2, result',
+                        [
+                            (7,3,10),
+                            ("Hello ", "World", "Hello World"),
+                            (10.5,25.5,36)
+                            ]
+                            )
+
+def test_add(num1, num2, result):
+    assert math_func.add(num1, num2) == result
